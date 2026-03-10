55ecbb94ef008dffbededed8a1d5794e7fd72d59
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-03-10 08:37:09 +0100

    feat: input tests

--- src/tui/state/input.rs
+++ src/tui/state/input.rs
@@ -33,7 +33,8 @@ impl Input {
         if self.cursor == 0 {
             return;
         }
-        self.cursor -= 1;
+
+        self.cursor = self.clamp(self.cursor).saturating_sub(1);
 
         let idx = self.byte_index();
         self.buffer.remove(idx);
@@ -78,5 +79,220 @@ impl Input {
 
     fn clamp(&self, idx: usize) -> usize {
         idx.clamp(0, self.buffer.chars().count())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Input;
+    use rstest::rstest;
+
+    #[test]
+    fn set_cursor_after_the_text() {
+        let mut input = Input::default();
+        let text = "Lorem Ipsum";
+
+        input.set(text);
+        assert_eq!(input.buffer, text);
+        assert_eq!(input.cursor, text.chars().count());
+    }
+
+    #[test]
+    fn clears_buffer() {
+        let mut input = Input::default();
+        let text = "Lorem Ipsum";
+
+        input.set(text);
+
+        assert_eq!(input.buffer, text);
+        input.clear();
+
+        assert!(input.buffer.is_empty());
+        assert_eq!(input.cursor, 0);
+    }
+
+    #[rstest]
+    #[case(0, "aLorem Ipsum")]
+    #[case(1, "Laorem Ipsum")]
+    #[case(2, "Loarem Ipsum")]
+    #[case(3, "Loraem Ipsum")]
+    #[case(4, "Loream Ipsum")]
+    #[case(5, "Lorema Ipsum")]
+    #[case(6, "Lorem aIpsum")]
+    #[case(7, "Lorem Iapsum")]
+    #[case(8, "Lorem Ipasum")]
+    #[case(9, "Lorem Ipsaum")]
+    #[case(10, "Lorem Ipsuam")]
+    #[case(11, "Lorem Ipsuma")]
+    fn puts_char_before_cursor(#[case] cursor: usize, #[case] expected: &str) {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = cursor;
+
+        input.put_char('a');
+        assert_eq!(input.buffer, expected);
+        assert_eq!(input.cursor, cursor + 1);
+    }
+
+    #[test]
+    fn puts_char_oob() {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = 42;
+
+        input.put_char('a');
+        assert_eq!(input.buffer, "Lorem Ipsuma");
+        assert_eq!(input.cursor, 12);
+    }
+
+    #[rstest]
+    #[case(1, "orem Ipsum")]
+    #[case(2, "Lrem Ipsum")]
+    #[case(3, "Loem Ipsum")]
+    #[case(4, "Lorm Ipsum")]
+    #[case(5, "Lore Ipsum")]
+    #[case(6, "LoremIpsum")]
+    #[case(7, "Lorem psum")]
+    #[case(8, "Lorem Isum")]
+    #[case(9, "Lorem Ipum")]
+    #[case(10, "Lorem Ipsm")]
+    #[case(11, "Lorem Ipsu")]
+    fn removes_char_before_cursor(#[case] cursor: usize, #[case] expected: &str) {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = cursor;
+
+        input.remove_char();
+        assert_eq!(input.buffer, expected);
+        assert_eq!(input.cursor, cursor - 1);
+    }
+
+    #[test]
+    fn removing_first_char_does_nothing() {
+        let mut input = Input::default();
+        let text = "Lorem Ipsum";
+        input.set(text);
+        input.cursor = 0;
+
+        input.remove_char();
+        assert_eq!(input.buffer, text);
+        assert_eq!(input.cursor, 0);
+    }
+
+    #[test]
+    fn remove_char_oob() {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = 42;
+
+        input.remove_char();
+        assert_eq!(input.buffer, "Lorem Ipsu");
+        assert_eq!(input.cursor, 10);
+    }
+
+    #[test]
+    fn cursor_left_move_left() {
+        // duh
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor_left();
+
+        assert_eq!(input.cursor, 10);
+    }
+
+    #[test]
+    fn cursor_left_at_the_beginning_does_nothing() {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = 0;
+        input.cursor_left();
+
+        assert_eq!(input.cursor, 0);
+    }
+
+    #[test]
+    fn cursor_left_oob() {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = 42;
+        input.cursor_left();
+
+        assert_eq!(input.cursor, 11);
+    }
+
+    #[test]
+    fn cursor_right_moves_right() {
+        // duh
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = 0;
+        input.cursor_right();
+
+        assert_eq!(input.cursor, 1);
+    }
+
+    #[test]
+    fn cursor_right_at_the_end_does_nothing() {
+        // duh
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        let cursor = input.cursor;
+        input.cursor_right();
+
+        assert_eq!(input.cursor, cursor);
+    }
+
+    #[test]
+    fn cursor_right_oob() {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = 42;
+        input.cursor_right();
+
+        assert_eq!(input.cursor, 11);
+    }
+
+    #[rstest]
+    fn cursor_start(#[values(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 42)] initial: usize) {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = initial;
+        input.cursor_start();
+
+        assert_eq!(input.cursor, 0);
+    }
+
+    #[rstest]
+    fn cursor_end(#[values(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 42)] initial: usize) {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = initial;
+        input.cursor_end();
+
+        assert_eq!(input.cursor, 11);
+    }
+
+    #[rstest]
+    #[case(0, "Lorem Ipsum")]
+    #[case(1, "orem Ipsum")]
+    #[case(2, "rem Ipsum")]
+    #[case(3, "em Ipsum")]
+    #[case(4, "m Ipsum")]
+    #[case(5, " Ipsum")]
+    #[case(6, "Ipsum")]
+    #[case(7, "psum")]
+    #[case(8, "sum")]
+    #[case(9, "um")]
+    #[case(10, "m")]
+    #[case(11, "")]
+    #[case(42, "")] // oob
+    fn remove_till_start(#[case] cursor: usize, #[case] expected: &str) {
+        let mut input = Input::default();
+        input.set("Lorem Ipsum");
+        input.cursor = cursor;
+        input.remove_till_start();
+
+        assert_eq!(input.buffer, expected);
+        assert_eq!(input.cursor, 0);
     }
 }
