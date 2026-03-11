d67fe2abc14eb0b7c1fb37b5a0c2bc36cfa817c9
Dominik Pilipczuk <63516830+snickerdoodle2@users.noreply.github.com>
2026-03-11 13:28:53 +0100

    feat: rename and delete modals

--- src/tui/layout.rs
+++ src/tui/layout.rs
@@ -45,7 +45,7 @@ impl<'a> Layout<'a> {
     fn large_screen(&self, area: Rect, buf: &mut Buffer) -> (bool, Option<(u16, u16)>) {
         let layout = layout::Layout::vertical(constraints![*=1, ==1]).split(area);
         Keybinds::new(self.state, &self.config.theme).render(layout[1], buf);
-        let layout = self.large_layout(layout[0], buf);
+        let layout = self.large_layout(layout[0]);
 
         let mut cursor = None;
 
@@ -70,7 +70,7 @@ impl<'a> Layout<'a> {
         (true, cursor)
     }
 
-    fn large_layout(&self, area: Rect, buf: &mut Buffer) -> Rc<[Rect]> {
+    fn large_layout(&self, area: Rect) -> Rc<[Rect]> {
         let layout = if cfg!(feature = "debug") {
             layout::Layout::horizontal(constraints![*=1, *=1, *=1])
         } else {
@@ -95,8 +95,8 @@ impl<'a> Layout<'a> {
             Mode::Normal | Mode::Search => self.render_normal(area, buf),
             Mode::Details => self.render_details(area, buf),
             Mode::Rename => self.render_rename(area, buf),
-            Mode::Create => None,
-            Mode::Delete => None,
+            Mode::Create => self.render_create(area, buf),
+            Mode::Delete => self.render_delete(area, buf),
             #[cfg(feature = "debug")]
             Mode::Debug => self.render_debug(area, buf),
         };
@@ -187,6 +187,36 @@ impl<'a> Layout<'a> {
         input.render(layout[1].inner(Margin::new(1, 0)), buf);
 
         Some(input.cursor())
+    }
+
+    fn render_create(&self, area: Rect, buf: &mut Buffer) -> Option<(u16, u16)> {
+        let layout = layout::Layout::vertical(constraints![== 1, == 1])
+            .spacing(1)
+            .split(area.centered_vertically(constraint![== 3]));
+
+        Paragraph::new("New session name")
+            .wrap(Wrap { trim: true })
+            .centered()
+            .render(layout[0], buf);
+
+        let mut input = Input::new(self.state.create_input(), &self.config.theme);
+        input.render(layout[1].inner(Margin::new(1, 0)), buf);
+
+        Some(input.cursor())
+    }
+
+    fn render_delete(&self, area: Rect, buf: &mut Buffer) -> Option<(u16, u16)> {
+        let area = area.centered_vertically(constraint![== 1]);
+
+        Paragraph::new(format!(
+            r#"Are you sure you want to delete "{}"?"#,
+            self.state.current().map(|s| s.name()).unwrap_or_default()
+        ))
+        .wrap(Wrap { trim: true })
+        .centered()
+        .render(area, buf);
+
+        None
     }
 
     fn render_details(&self, area: Rect, buf: &mut Buffer) -> Option<(u16, u16)> {
